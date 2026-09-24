819ea9d6490c78501944a5794771f3d5901b943c
AsiaWu_a <wuasia2007@hotmail.com>
2026-09-24 19:53:51 +0800

    part three

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("part one");
         System.out.println("part two");
+        System.out.println("part three");
     }
 }
